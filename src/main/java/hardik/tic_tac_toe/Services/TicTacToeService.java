31e76d921f842a6d60dd2a3a9a4c70deda122104
pachgadehardik <pachgadehardik@gmail.com>
2020-10-08 10:15:02 +0530

    Assign Letter to the player and Computer

--- src/main/java/hardik/tic_tac_toe/Services/TicTacToeService.java
+++ src/main/java/hardik/tic_tac_toe/Services/TicTacToeService.java
@@ -4,13 +4,11 @@ public class TicTacToeService {
 
 	public char[] createBoard() {
 		char[] board = new char[10];
-		for(int i = 0;i<board.length;i++) {
+		for(int i = 1;i<=board.length;i++) {
 			board[i] = ' ';
 		}
 		return board;
 	}
-	
-	
 	public void displayBoard(char[] arr) {
 		for(int i=0;i<arr.length;i++) {
 			System.out.println(arr[i]+" ");
--- src/main/java/hardik/tic_tac_toe/TicTacToeGame.java
+++ src/main/java/hardik/tic_tac_toe/TicTacToeGame.java
@@ -1,13 +1,58 @@
 package hardik.tic_tac_toe;
 
+import java.util.Scanner;
+
 import hardik.tic_tac_toe.Services.TicTacToeService;
 
 public class TicTacToeGame {
 
+	
+	public static char[] createBoard() {
+		char[] board = new char[10];
+		for(int i = 1;i<board.length;i++) {
+			board[i] = ' ';
+		}
+		return board;
+	}
+	public static void displayBoard(char[] arr) {
+		for(int i=0;i<arr.length;i++) {
+			System.out.println(arr[i]+" ");
+		}
+	}
+	
+	
+	public static char inputTheLetter() {
+		
+		System.out.println("Enter the Input: ");
+		Scanner inputOption = new Scanner(System.in);
+		char input = inputOption.next().charAt(0);
+		return input;
+	}
+	
+	public static void determinePlayerOption(char input) {
+		switch(input) {
+		case 'X':
+			System.out.println("Player 1 is having X");
+			System.out.println("Computer is having O");
+			break;
+		case 'O':
+			System.out.println("Player 1 is having O");
+			System.out.println("Computer is having X");
+			break;
+		default:
+			System.out.println("Invalid Input !!");
+			break;
+		}
+	}
+	
+	
+	
 	public static void main(String args[]) {
 		System.out.println("Welcome to Tic Tac toe game!!");
-		TicTacToeService service = new TicTacToeService();
 		// creating a board
-		service.displayBoard(service.createBoard());
+		displayBoard(createBoard());
+		char player1Input = inputTheLetter();
+		determinePlayerOption(player1Input);
+		
 	}
 }
