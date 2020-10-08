9333558eeda4d9cda579570c1a49982b0f5cd503
pachgadehardik <pachgadehardik@gmail.com>
2020-10-08 10:16:50 +0530

    Assign Letter to the player and Computer

--- src/main/java/hardik/tic_tac_toe/TicTacToeGame.java
+++ src/main/java/hardik/tic_tac_toe/TicTacToeGame.java
@@ -6,31 +6,30 @@ import hardik.tic_tac_toe.Services.TicTacToeService;
 
 public class TicTacToeGame {
 
-	
 	public static char[] createBoard() {
 		char[] board = new char[10];
-		for(int i = 1;i<board.length;i++) {
+		for (int i = 1; i < board.length; i++) {
 			board[i] = ' ';
 		}
 		return board;
 	}
+
 	public static void displayBoard(char[] arr) {
-		for(int i=0;i<arr.length;i++) {
-			System.out.println(arr[i]+" ");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i] + " ");
 		}
 	}
-	
-	
+
 	public static char inputTheLetter() {
-		
+
 		System.out.println("Enter the Input: ");
 		Scanner inputOption = new Scanner(System.in);
 		char input = inputOption.next().charAt(0);
 		return input;
 	}
-	
+
 	public static void determinePlayerOption(char input) {
-		switch(input) {
+		switch (input) {
 		case 'X':
 			System.out.println("Player 1 is having X");
 			System.out.println("Computer is having O");
@@ -44,15 +43,13 @@ public class TicTacToeGame {
 			break;
 		}
 	}
-	
-	
-	
+
 	public static void main(String args[]) {
 		System.out.println("Welcome to Tic Tac toe game!!");
 		// creating a board
 		displayBoard(createBoard());
 		char player1Input = inputTheLetter();
 		determinePlayerOption(player1Input);
-		
+
 	}
 }
