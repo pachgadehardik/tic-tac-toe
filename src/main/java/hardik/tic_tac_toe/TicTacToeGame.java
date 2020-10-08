22086e84e874872fc2b14da5f9effa0c5c792f5e
pachgadehardik <pachgadehardik@gmail.com>
2020-10-08 10:23:12 +0530

    Assign Letter to the player and Computer

--- src/main/java/hardik/tic_tac_toe/TicTacToeGame.java
+++ src/main/java/hardik/tic_tac_toe/TicTacToeGame.java
@@ -6,6 +6,7 @@ import hardik.tic_tac_toe.Services.TicTacToeService;
 
 public class TicTacToeGame {
 
+	//Creates a Board
 	public static char[] createBoard() {
 		char[] board = new char[10];
 		for (int i = 1; i < board.length; i++) {
@@ -19,29 +20,31 @@ public class TicTacToeGame {
 			System.out.println(arr[i] + " ");
 		}
 	}
-
+	//Input the Character
 	public static char inputTheLetter() {
 
 		System.out.println("Enter the Input: ");
-		Scanner inputOption = new Scanner(System.in);
-		char input = inputOption.next().charAt(0);
+		Scanner scanner = new Scanner(System.in);
+		char input = scanner.next().charAt(0);
 		return input;
 	}
-
-	public static void determinePlayerOption(char input) {
+	
+	//Determine Player's option 
+	public static char determinePlayerOption(char input) {
 		switch (input) {
 		case 'X':
 			System.out.println("Player 1 is having X");
 			System.out.println("Computer is having O");
-			break;
+			return 'O';
 		case 'O':
 			System.out.println("Player 1 is having O");
 			System.out.println("Computer is having X");
-			break;
+			return 'X';
 		default:
 			System.out.println("Invalid Input !!");
 			break;
 		}
+		return ' ';
 	}
 
 	public static void main(String args[]) {
@@ -49,6 +52,7 @@ public class TicTacToeGame {
 		// creating a board
 		displayBoard(createBoard());
 		char player1Input = inputTheLetter();
+		char Player2Input = determinePlayerOption(player1Input);
 		determinePlayerOption(player1Input);
 
 	}
